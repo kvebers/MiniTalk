88d7234cc788bd44fa4ff8e540ae5fc8e30295d1
kvebers <kvebers@student.42heilbronn.de>
2023-01-09 12:45:39 +0100

    Fin

--- src_c/client.c
+++ src_c/client.c
@@ -6,53 +6,72 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 12:16:13 by kvebers           #+#    #+#             */
-/*   Updated: 2023/01/08 19:54:11 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/01/09 12:38:27 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-void	test1(int pid, char c)
+void	ft_test1(int pid, char c)
 {
 	if (c / 2 > 0)
-		test1(pid, c / 2);
+		ft_test1(pid, c / 2);
 	if (c % 2 == 1)
 		kill(pid, SIGUSR1);
 	else
 		kill(pid, SIGUSR2);
-	sleep (1);
-	
+	usleep (50);
 }
 
-void	test(int pid, char c)
+void	ft_test(int pid, char c)
 {
-	int base;
+	int	base;
 
 	base = 128;
 	while (base > c)
 	{
 		kill(pid, SIGUSR2);
 		base = base / 2;
-		sleep (1);
+		usleep (50);
 	}
-	test1(pid, c);
+	ft_test1(pid, c);
+}
+
+int	ft_check_arg(char *arg)
+{
+	int	cnt;
+
+	cnt = 0;
+	while (*(arg + cnt) != '\0')
+	{
+		if (ft_isprint(*(arg + cnt)) == 0)
+		{
+			ft_printf("Error: The char is not printable");
+			return (0);
+		}
+		cnt++;
+	}
+	return (1);
 }
 
 int	main(int argc, char **argv)
 {
-	int pid;
-	int cnt;
-	
+	int	pid;
+	int	cnt;
+
 	if (argc != 3)
 	{
-		ft_printf("call should be ./client PID + TXT");
-		return 0;
+		ft_printf("Error: Call should be ./client PID + TXT");
+		return (0);
 	}
 	pid = ft_atoi(argv[1]);
 	cnt = 0;
+	if (ft_check_arg(argv[2]) == 0)
+		return (0);
 	while (*(argv[2] + cnt) != '\0')
 	{
-		test(pid, *(argv[2] + cnt));
+		ft_test(pid, *(argv[2] + cnt));
 		cnt++;
 	}
+	return (0);
 }
--- src_s/server.c
+++ src_s/server.c
@@ -6,38 +6,35 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 12:15:40 by kvebers           #+#    #+#             */
-/*   Updated: 2023/01/08 19:32:55 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/01/09 12:27:56 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-void printer(int sig)
+void	ft_printer(int sig)
 {
-    static int  result;
-    static int  pow;
+	static int	result;
+	static int	pow;
 
-    if (pow == 0 || pow == 1)
-        pow = 256;
-    pow = pow / 2;
-    if (sig == SIGUSR1)
-        result = result + pow;
-    ft_printf("\nPow: %i Result %i", pow, result);
-    if (pow == 1)
-    {
-        ft_putchar(result);
-        result = 0;
-        ft_printf("\n");
-    }
+	if (pow == 0 || pow == 1)
+		pow = 256;
+	pow = pow / 2;
+	if (sig == SIGUSR1)
+		result = result + pow;
+	if (pow == 1)
+	{
+		ft_putchar(result);
+		result = 0;
+	}
 }
 
-
-int main(void) 
+int	main(void)
 {
-    ft_printf("Server PID: %d\n", getpid());
-    signal(SIGUSR1, printer);
-    signal(SIGUSR2, printer);
-    while (1) 
-        pause();
-    return 0;
+	ft_printf("Server PID: %d\n", getpid());
+	signal(SIGUSR1, ft_printer);
+	signal(SIGUSR2, ft_printer);
+	while (1)
+		pause();
+	return (0);
 }
